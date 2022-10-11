cfbafb51dccd97b03d2c6be8dd6d46090ea3344c
E2-Priya <109967357+E2-Priya@users.noreply.github.com>
2022-10-11 17:32:04 +0530

    Add files via upload

---
+++ MapStudent.java
@@ -0,0 +1,64 @@
+package studentInformation;
+
+public class MapStudent {
+	private int id;
+	private String studentName;
+	private int age;
+	private int phone;
+	private String address;
+	private int gender;
+	private int subId;
+	
+	
+	public int getId() {
+		return id;
+	}
+	public String getStudentName() {
+		return studentName;
+	}
+	public int getAge() {
+		return age;
+	}
+	public int getPhone() {
+		return phone;
+	}
+	public String getAddress() {
+		return address;
+	}
+	public int getGender() {
+		return gender;
+	}
+	public int getSubId() {
+		return subId;
+	}
+	public int getMark() {
+		return mark;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public void setStudentName(String studentName) {
+		this.studentName = studentName;
+	}
+	public void setAge(int age) {
+		this.age = age;
+	}
+	public void setPhone(int phone) {
+		this.phone = phone;
+	}
+	public void setAddress(String address) {
+		this.address = address;
+	}
+	public void setGender(int gender) {
+		this.gender = gender;
+	}
+	public void setSubId(int subId) {
+		this.subId = subId;
+	}
+	public void setMark(int mark) {
+		this.mark = mark;
+	}
+	private int mark;
+	
+
+}
